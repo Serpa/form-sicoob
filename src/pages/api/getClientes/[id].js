ec30e1980fe75ef5614fa50fc79a95899a8e4203
Guilherme Serpa Batista <serpa419@gmail.com>
2023-03-01 11:36:19 -0300

    register update

--- src/pages/api/getClientes/[id].js
+++ src/pages/api/getClientes/[id].js
@@ -2,6 +2,7 @@ import prisma from '../../../../lib/prisma'
 
 export default async function GetClientes(req, res) {
     const { id } = req.query
+    if (id === "undefined") res.status(200).json(null)
     const result = await prisma.clientes.findMany({
         where: {
             assembleiaId: id
